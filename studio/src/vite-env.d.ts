e123235c159a9956dfc5940eb6e7e2ddec9b5f88
Max Boone <contact@maxboone.com>
2024-01-17 20:33:46 +0100

    feat(studio): rebuild dependencies

--- studio/src/vite-env.d.ts
+++ studio/src/vite-env.d.ts
@@ -1,1 +1,3 @@
+// vite-env.d.ts
+/// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite/client" />
--- studio/vite.config.ts
+++ studio/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import Pages from 'vite-plugin-pages'
+import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: {
+      exclude: ['@codemirror/state'],
+  },
+  plugins: [
+    svgr(),
+    Pages(),
+    react(),
+  ],
 })
