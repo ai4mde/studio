539a41a4d97509a77dc483e7bf9690ee3d2da7b0
Max Boone <contact@maxboone.com>
2024-02-09 23:33:01 +0100

    fix(diagram): fix text size of methods and attributes

--- studio/src/lib/features/diagram/components/nodes/class/ClassNode/ClassNode.tsx
+++ studio/src/lib/features/diagram/components/nodes/class/ClassNode/ClassNode.tsx
@@ -5,14 +5,14 @@ import NodeWrapper from "../../shared/NodeWrapper";
 const ClassNode: React.FC<NodeProps> = (node) => {
     return (
         <NodeWrapper node={node} selected={node.selected}>
-            <div className="font-mono border border-solid border-black bg-white flex flex-col text-center">
-                <span className="py-2 px-6 font-bold">{node.data?.name}</span>
+            <div className="flex flex-col border border-solid border-black bg-white text-center font-mono">
+                <span className="px-6 py-2 font-bold">{node.data?.name}</span>
                 <div className="flex flex-col border-t border-solid border-black p-1">
                     {node.data?.attributes?.map(
                         (attribute: any, idx: number) => (
                             <div
                                 key={`attribute-${node.id}-${idx}`}
-                                className="flex flex-row align-center justify-between gap-2 px-1 text-sm"
+                                className="align-center flex flex-row justify-between gap-2 px-1 text-sm"
                             >
                                 <span>
                                     {attribute?.derived ? "/" : "+"}
@@ -27,7 +27,7 @@ const ClassNode: React.FC<NodeProps> = (node) => {
                     {node.data?.methods?.map((method: any, idx: number) => (
                         <div
                             key={`method-${node.id}-${idx}`}
-                            className="flex flex-row align-center justify-between gap-1 px-1 text-xs"
+                            className="align-center flex flex-row justify-between gap-1 px-1 text-sm"
                         >
                             <span>+{method?.name}()</span>
                             <span>:</span>
