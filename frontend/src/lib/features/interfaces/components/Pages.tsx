d3721ef7cd72726fe27025a08d1e3cef9a5fb019
syvandam <s.y.van.dam@umail.leidenuniv.nl>
2024-06-26 12:55:32 +0200

    [Feat] extend page editor in interface tab with section component select & more structured localStorage json

--- frontend/src/lib/features/interfaces/components/Pages.tsx
+++ frontend/src/lib/features/interfaces/components/Pages.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Plus, Trash, Save, Ban } from "lucide-react";
+import { Plus, Trash, Save, Ban, Pencil } from "lucide-react";
 import useLocalStorage from './useLocalStorage';
 import {
     FormControl,
@@ -8,6 +8,7 @@ import {
     Option,
     Divider,
 } from "@mui/joy"
+import Multiselect from 'multiselect-react-dropdown';
 
 type Props = {
     projectId: string;
@@ -22,12 +23,15 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
     const [newName, setNewName] = useState('');
     const [categories, , isSuccessCategories] = useLocalStorage('categories', []);
     const [selectedCategory, setSelectedCategory] = useLocalStorage('selectedCategory', '');
+    const [sections, isSuccessSections] = useLocalStorage('sections', []);
+    const [selectedSections, setSelectedSections] = useLocalStorage('selectedSections', []);
+    const [pencelClick, setPencelClick] = useState(false);
 
     useEffect(() => {
-        if (editIndex !== -1 && data[editIndex].category) {
-            setSelectedCategory(data[editIndex].category);
+        if (editIndex !== -1 && data[editIndex].sections) {
+            setSelectedSections(data[editIndex].sections);
         } else {
-            setSelectedCategory('');
+            setSelectedSections([]);
         }
     }, [editIndex, data]);
 
@@ -40,11 +44,11 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
         const newData = [...data];
         newData[index].name = newName;
         newData[index].category = selectedCategory;
+        newData[index].sections = selectedSections;
         setData(newData);
-        setEditIndex(-1);
     };
 
-    const handleCancel = () => {
+    const handleMinus = () => {
         setEditIndex(-1);
     };
 
@@ -52,8 +56,11 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
         setNewName(event.target.value);
     };
 
-    const handleCategoryChange = (event, newValue) => {
-        setSelectedCategory(newValue);
+    const handleCategoryChange = (event, newValue, pageIndex: number) => {
+        setSelectedCategory(newValue as string);
+        const newData = [...data];
+        newData[pageIndex].category = newValue;
+        setData(newData);
     };
 
     const handleDelete = (index: number) => {
@@ -63,6 +70,40 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
         setEditIndex(-1);
     };
 
+    
+    const handleSectionSelect = (selectedList, selectedItem, pageIndex: sint) => {
+        if (!selectedSections) {
+            setSelectedSections([]);
+        }
+        selectedSections.push(selectedItem);
+        setSelectedSections(selectedSections);
+        handleSave(pageIndex);
+    };
+
+    const handleSectionRemove = (selectedList, selectedItem, pageIndex: int) => {
+        if (!selectedSections) {
+            setSelectedSections([]);
+        }
+        selectedSections.pop(selectedItem);
+        setSelectedSections(selectedSections);
+        handleSave(pageIndex);
+    };
+
+    const handlePencilClick = () => {
+        setPencelClick(true);
+    }
+
+    const handleNameChange = (index: number) => {
+        const newData = [...data];
+        newData[index].name = newName;
+        setData(newData);
+        setPencelClick(false);
+    };
+
+    const handleNameCancel = () => {
+        setPencelClick(false);
+    };
+
     return (
         <>
             <div className="flex flex-wrap gap-4">
@@ -70,15 +111,42 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
                     data.map((page, index) => (
                         <div key={index} className="flex flex-col gap-2">
                             {editIndex === index ? (
-                                <div className="flex flex-col gap-2 space-y-3">
-                                    <FormControl required className="space-y-1">
+                                <div className="w-[240px] flex flex-col gap-2 space-y-2">
+                                    <div>
                                         <h3 className="text-xl font-bold">Name</h3>
-                                        <Input 
-                                            type="text" 
-                                            value={newName}
-                                            onChange={handleInputChange}
-                                        />
-                                    </FormControl>
+                                        {!pencelClick && (
+                                            <div className="flex flex-wrap gap-2">
+                                                <h2 className="text-l">{page.name}</h2>
+                                                <Pencil
+                                                    className="cursor-pointer ml-auto"
+                                                    onClick={handlePencilClick}
+                                                />
+                                            </div>
+                                        )}
+                                        {pencelClick && (
+                                            <FormControl required className="space-y-1">
+                                                <Input 
+                                                    type="text" 
+                                                    value={newName}
+                                                    onChange={handleInputChange}
+                                                />
+                                                <div className="flex flex-wrap gap-2 ml-auto">
+                                                    <button
+                                                        onClick={() => handleNameChange(index)}
+                                                        className="w-[40px] h-[40px] bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600"
+                                                    >
+                                                        <Save />
+                                                    </button>
+                                                    <button
+                                                        onClick={handleNameCancel}
+                                                        className="w-[40px] h-[40px] bg-gray-300 text-gray-700 px-2 py-1 rounded-md hover:bg-gray-400"
+                                                    >
+                                                        <Ban />
+                                                    </button>
+                                                </div>
+                                            </FormControl>
+                                        )}
+                                    </div>
                                     {isSuccessCategories && (
                                         <FormControl className="space-y-1">
                                             <h3 className="text-xl font-bold">Category</h3>
@@ -86,7 +154,7 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
                                                 <Select 
                                                     placeholder="Choose one…"
                                                     value={selectedCategory}
-                                                    onChange={handleCategoryChange}
+                                                    onChange={(event, newValue) => handleCategoryChange(event, newValue, index)}
                                                 >
                                                     {categories.map((e) => (
                                                         <Option key={e.name} value={e.name}>{e.name}</Option>
@@ -97,14 +165,23 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
                                             )}
                                         </FormControl>
                                     )}
+                                    {isSuccessSections && (
+                                        <div className='space-y-1'>
+                                            <h3 className="text-xl font-bold">Section Components</h3>
+                                            <Multiselect
+                                                options={sections}
+                                                displayValue='name'
+                                                placeholder="Select sections..."
+                                                showCheckbox={true}
+                                                style={{chips:{background:'rgb(231 229 228)',color:'rgb(61 56 70)'}}}
+                                                selectedValues={selectedSections}
+                                                onSelect={(selectedList, selectedItem) => handleSectionSelect(selectedList, selectedItem, index)}
+                                                onRemove={(selectedList, selectedItem) => handleSectionRemove(selectedList, selectedItem, index)}
+                                            />
+                                        </div>
+                                    )}
                                     <Divider />
                                     <div className="flex gap-2">
-                                        <button
-                                            onClick={() => handleSave(index)}
-                                            className="w-[40px] h-[40px] bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600"
-                                        >
-                                            <Save />
-                                        </button>
                                         <button
                                             onClick={() => handleDelete(index)}
                                             className="w-[40px] h-[40px] bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600"
@@ -112,10 +189,10 @@ export const Pages: React.FC<Props> = ({ app_comp }) => {
                                             <Trash />
                                         </button>
                                         <button
-                                            onClick={handleCancel}
-                                            className="w-[40px] h-[40px] bg-gray-300 text-gray-700 px-2 py-1 rounded-md hover:bg-gray-400"
+                                            onClick={handleMinus}
+                                            className="w-[60px] h-[40px] bg-stone-200 text-stone-900 px-2 py-1 rounded-md hover:bg-blue-600"
                                         >
-                                            <Ban />
+                                            Close
                                         </button>
                                     </div>
                                 </div>
