c2220bd1c310eb9d121954d1fb7af9d890e9d7ca
syvandam <s.y.van.dam@umail.leidenuniv.nl>
2024-06-26 19:44:00 +0200

    [Fix] more structred styling json in localstorage

--- frontend/src/lib/features/interfaces/components/Styling.tsx
+++ frontend/src/lib/features/interfaces/components/Styling.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
-    Button,
-    Divider,
     FormControl,
     Input,
     Select,
@@ -20,33 +18,49 @@ type Props = {
 
 export const Styling: React.FC<Props> = ({ app_comp }) => {
 
-    const [radius, setRadius] = useLocalStorage('radius', 0);
-    const [backgroundColor, setBackgroundColor] = useLocalStorage('backgroundColor', "#000000");
-    const [textColor, setTextColor] = useLocalStorage('textColor', "#FFFFFF");
-    const [accentColor, setAccentColor] = useLocalStorage('accentColor', "#777777");
-    const [selectedStyle, setSelectedStyle] = useLocalStorage('selectedStyle', '');
+    const [data, setData, isSuccess] = useLocalStorage('styling', '');
     const [logoFile, setLogoFile] = useLocalStorage('logoFile', null);
     const [logoDimensions, setLogoDimensions] = useLocalStorage('logoDimensions', { width: 0, height: 0 });
+
+    useEffect(() => {
+        if (!data || Object.keys(data).length === 0) {
+            const defaultStyling = {
+                radius: 0,
+                backgroundColor: '#FFFFFF',
+                textColor: '#000000',
+                accentColor: '#777777',
+                selectedStyle: 'basic'
+            };
+            setData(defaultStyling);
+        }
+    }, [data, setData]);
 
     const handleRadiusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = parseInt(event.target.value, 10);
         if (value >= 0 && value <= 10) {
-            setRadius(value);
+            const newData = { ...data, radius: value };
+            setData(newData);
         }
     }
+
     const handleBackgroundColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBackgroundColor(event.target.value);
+        const newData = { ...data, backgroundColor: event.target.value };
+        setData(newData);
     }
 
     const handleTextColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTextColor(event.target.value);
+        const newData = { ...data, textColor: event.target.value };
+        setData(newData);
     }
 
     const handleAccentColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAccentColor(event.target.value);
+        const newData = { ...data, accentColor: event.target.value };
+        setData(newData);
     }
+
     const handleStyleChange = (event, newValue) => {
-        setSelectedStyle(newValue);
+        const newData = { ...data, selectedStyle: newValue };
+        setData(newData);
     };
 
     const handleLogoChange = (event) => {
@@ -73,110 +87,111 @@ export const Styling: React.FC<Props> = ({ app_comp }) => {
         }
     };
 
-
     return (
         <>
-            <div className="max-w-[300px] mx-auto mt-4 ml-2 space-y-4">
-                <FormControl>
-                    <h3 className="text-xl font-bold">Logo</h3>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        onChange={handleLogoChange}
-                        className="hidden"
-                        id="logoInput"
-                    />
-                    <label htmlFor="logoInput" className="cursor-pointer">
-                        <Plus />
-                        {logoFile ? `${logoFile.name} (${logoDimensions.width}x${logoDimensions.height}px)` : ''}
-                    </label>
-                </FormControl>
-
-                <FormControl required>
-                    <h3 className="text-xl font-bold">Style</h3>
-                    <Select
-                        placeholder="Choose one…"
-                        value={selectedStyle}
-                        onChange={handleStyleChange}
-                    >
-                        <Option value="basic">Basic</Option>
-                        <Option value="modern">Modern</Option>
-                        <Option value="abstract">Abstract</Option>
-                    </Select>
-                </FormControl>
-
-                <FormControl>
-                    <h3 className="text-xl font-bold">Background color</h3>
-                    <div className="flex w-full flex-row justify-between pb-1">
-                        <Input 
-                            type="text" 
-                            value={backgroundColor}
-                            onChange={handleBackgroundColorChange}
-                            pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
-                            placeholder="#777777"
-                        />
+            { isSuccess && (
+                <div className="max-w-[300px] mx-auto mt-4 ml-2 space-y-4">
+                    <FormControl>
+                        <h3 className="text-xl font-bold">Logo</h3>
                         <input
-                            type="color" 
-                            value={backgroundColor}
-                            onChange={handleBackgroundColorChange}
-                            className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
-                            style={{ backgroundColor: backgroundColor }}
+                            type="file"
+                            accept="image/*"
+                            onChange={handleLogoChange}
+                            className="hidden"
+                            id="logoInput"
                         />
-                    </div>
-                </FormControl>
+                        <label htmlFor="logoInput" className="cursor-pointer">
+                            <Plus />
+                            {logoFile ? `${logoFile.name} (${logoDimensions.width}x${logoDimensions.height}px)` : ''}
+                        </label>
+                    </FormControl>
 
-                <FormControl>
-                    <h3 className="text-xl font-bold">Text color</h3>
-                    <div className="flex w-full flex-row justify-between pb-1">
+                    <FormControl required>
+                        <h3 className="text-xl font-bold">Style</h3>
+                        <Select
+                            placeholder="Choose one…"
+                            value={data.selectedStyle}
+                            onChange={handleStyleChange}
+                        >
+                            <Option value="basic">Basic</Option>
+                            <Option value="modern">Modern</Option>
+                            <Option value="abstract">Abstract</Option>
+                        </Select>
+                    </FormControl>
+
+                    <FormControl>
+                        <h3 className="text-xl font-bold">Background color</h3>
+                        <div className="flex w-full flex-row justify-between pb-1">
+                            <Input 
+                                type="text" 
+                                value={data.backgroundColor}
+                                onChange={handleBackgroundColorChange}
+                                pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+                                placeholder="#FFFFFF"
+                            />
+                            <input
+                                type="color" 
+                                value={data.backgroundColor}
+                                onChange={handleBackgroundColorChange}
+                                className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
+                                style={{ backgroundColor: data.backgroundColor }}
+                            />
+                        </div>
+                    </FormControl>
+
+                    <FormControl>
+                        <h3 className="text-xl font-bold">Text color</h3>
+                        <div className="flex w-full flex-row justify-between pb-1">
+                            <Input 
+                                type="text" 
+                                value={data.textColor}
+                                onChange={handleTextColorChange}
+                                pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+                                placeholder="#000000"
+                            />
+                            <input
+                                type="color" 
+                                value={data.textColor}
+                                onChange={handleTextColorChange}
+                                className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
+                                style={{ backgroundColor: data.textColor }}
+                            />
+                        </div>
+                    </FormControl>
+
+                    <FormControl>
+                        <h3 className="text-xl font-bold">Accent color</h3>
+                        <div className="flex w-full flex-row justify-between pb-1">
+                            <Input 
+                                type="text" 
+                                value={data.accentColor}
+                                onChange={handleAccentColorChange}
+                                pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+                                placeholder="#777777"
+                            />
+                            <input
+                                type="color" 
+                                value={data.accentColor}
+                                onChange={handleAccentColorChange}
+                                className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
+                                style={{ backgroundColor: data.accentColor }}
+                            />
+                        </div>
+                    </FormControl>
+
+                    <FormControl required>
+                        <h3 className="text-xl font-bold">Radius</h3>
                         <Input 
-                            type="text" 
-                            value={textColor}
-                            onChange={handleTextColorChange}
-                            pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
-                            placeholder="#777777"
+                            type="number"
+                            value={data.radius} 
+                            onChange={handleRadiusChange}
+                            min={0}
+                            max={10}
+                            required 
                         />
-                        <input
-                            type="color" 
-                            value={textColor}
-                            onChange={handleTextColorChange}
-                            className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
-                            style={{ backgroundColor: textColor }}
-                        />
-                    </div>
-                </FormControl>
-
-                <FormControl>
-                    <h3 className="text-xl font-bold">Accent color</h3>
-                    <div className="flex w-full flex-row justify-between pb-1">
-                        <Input 
-                            type="text" 
-                            value={accentColor}
-                            onChange={handleAccentColorChange}
-                            pattern="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
-                            placeholder="#777777"
-                        />
-                        <input
-                            type="color" 
-                            value={accentColor}
-                            onChange={handleAccentColorChange}
-                            className="w-[50px] h-[50px] border border-gray-500 p-0 cursor-pointer"
-                            style={{ backgroundColor: accentColor }}
-                        />
-                    </div>
-                </FormControl>
-
-                <FormControl required>
-                    <h3 className="text-xl font-bold">Radius</h3>
-                    <Input 
-                        type="number"
-                        value={radius} 
-                        onChange={handleRadiusChange}
-                        min={0}
-                        max={10}
-                        required 
-                    />
-                </FormControl>
-            </div>
+                    </FormControl>
+                </div>
+            )}
         </>
     )
 }
