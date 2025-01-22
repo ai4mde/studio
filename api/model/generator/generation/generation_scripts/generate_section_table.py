c8bfacd3297097cc6f5904e14dada89f9379ba92
Sven van Dam <s.y.van.dam@umail.leidenuniv.nl>
2025-01-22 14:29:18 +0100

    [Fix] remove legacy generation code

--- api/model/generator/generation/generation_scripts/generate_section_table.py
+++
@@ -1,9 +0,0 @@
-import sys
-from utils.loading_json_utils import get_apps
-
-def main():
-    if (len(sys.argv) >= 3 and str(sys.argv[1]) == "get_apps"):
-        print(get_apps(sys.argv[2]))
-
-if __name__ == "__main__":
-    main()
