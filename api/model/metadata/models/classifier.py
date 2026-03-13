986b20e26c8b400b60034b8f22e1bedd260c5d47
Maarten Verhoeff <maarten.verhoeff@caiway.net>
2026-03-13 12:18:22 +0100

    Extend Classifier ORM

--- api/model/metadata/models/classifier.py
+++ api/model/metadata/models/classifier.py
@@ -18,7 +18,7 @@ class ClassifierManager(models.Manager['Classifier']):
         *,
         type: str,
         project: Project,
-        extension_data: dict[type["ClassifierExtensionBase"], dict] | None = None,
+        extension_data: dict[type["ClassifierExtensionBase"], dict | list[dict]] | None = None,
     ):
         extension_data = extension_data or {}
 
@@ -32,9 +32,37 @@ class ClassifierManager(models.Manager['Classifier']):
         # This will throw an error if any of the extensions are not allowed
         # and the transaction will be rolled back, preventing incomplete classifiers from being created
         for model, data in extension_data.items():
-            model.objects.create(classifier=classifier, **data)
+            if isinstance(data, list):
+                if not model.allows_multiple():
+                    raise ValidationError(
+                        f"{model.__name__} does not allow multiple instances, but a list was provided"
+                    )
+                for item in data:
+                    model.objects.create(classifier=classifier, **item)
+            else:
+                model.objects.create(classifier=classifier, **data)
 
+        classifier.validate_completeness()
         return classifier
+
+    @transaction.atomic
+    def add_extensions(
+        self,
+        extension_data: dict[type["ClassifierExtensionBase"], dict | list[dict]],
+    ) -> None:
+        """
+            Add extensions to an existing classifier.
+        """
+        for model, data in extension_data.items():
+            if isinstance(data, list):
+                if not model.allows_multiple():
+                    raise ValidationError(
+                        f"{model.__name__} does not allow multiple instances, but a list was provided"
+                    )
+                for item in data:
+                    model.objects.create(classifier=self, **item)
+            else:
+                model.objects.create(classifier=self, **data)
 
 
 class Classifier(models.Model):
@@ -66,19 +94,29 @@ class Classifier(models.Model):
             else self.type
         )
 
-    REQUIRED_EXTENSIONS: dict[str, tuple[type[models.Model], ...]] = {}
+    REQUIRED_EXTENSIONS: dict[str, tuple[type["ClassifierExtensionBase"], ...]] = {}
 
+    # Just a copy, make sure to understand this to check if the logic checks out
     def validate_completeness(self) -> None:
         errors: dict[str, ValidationErrorMessageArg] = {}
         required = self.REQUIRED_EXTENSIONS.get(self.type, ())
     
         for model in required:
-            related_name = model.__name__.lower()
-            if not hasattr(self, related_name):
-                errors[related_name] = [
-                    f"{model.__name__} extension is required for {self.type} classifiers"
-                ]
+            accessor_name = model.get_classifier_accessor_name()
 
+            try:
+                relation = getattr(self, accessor_name)
+            except model.DoesNotExist:
+                errors[accessor_name] = [
+                    f"{model.__name__} extension is required for classifier type '{self.type}'"
+                ]
+                continue
+
+            if hasattr(relation, "exists") and not relation.exists():
+                errors[accessor_name] = [
+                    f"At least one {model.__name__} extension is required for classifier type '{self.type}'"
+                ]
+        
         if errors:
             raise ValidationError(errors)
 
@@ -97,21 +135,36 @@ class Classifier(models.Model):
 
 class ClassifierExtensionBase(models.Model):
     """
-        Base class to extend specific classifier types 
-        with additional attributes 
+        Base class for classifier extensions
+        Subclasses define how they reference the classifier
     """
-    classifier = models.OneToOneField(
-        Classifier,
-        on_delete=models.CASCADE,
-        primary_key=True,
-        related_name="%(class)s",
-    )
-
     # Each extension subclass should define its own allowed classifier types
     ALLOWED_CLASSIFIER_TYPES: tuple[str, ...] = ()
 
     class Meta:
         abstract = True
+
+    def get_classifier(self) -> "Classifier":
+        raise NotImplementedError(
+            f"{self.__class__.__name__} must implement get_classifier()"
+        )
+
+    @classmethod
+    def get_classifier_accessor_name(cls) -> str:
+        field = cls._meta.get_field("classifier")
+        related_name = field.remote_field.related_name
+
+        if related_name is None:
+            raise RuntimeError(
+                f"{cls.__name__} must define a related_name for the classifier field"
+            )
+
+        return related_name
+
+    @classmethod
+    def allows_multiple(cls) -> bool:
+        field = cls._meta.get_field("classifier")
+        return isinstance(field, models.ForeignKey)
 
     def clean(self):
         super().clean()
@@ -121,9 +174,11 @@ class ClassifierExtensionBase(models.Model):
                 f"{self.__class__.__name__} must define ALLOWED_CLASSIFIER_TYPES"
             )
 
-        if self.classifier.type not in self.ALLOWED_CLASSIFIER_TYPES:
+        classifier = self.get_classifier()
+
+        if classifier.type not in self.ALLOWED_CLASSIFIER_TYPES:
             raise ValidationError(
-                f"{self.__class__.__name__} not allowed for classifier type '{self.classifier.type}'"
+                f"{self.__class__.__name__} not allowed for classifier type '{classifier.type}'"
             )
 
     def save(self, *args, **kwargs):
@@ -131,21 +186,52 @@ class ClassifierExtensionBase(models.Model):
         return super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        required = self.classifier.REQUIRED_EXTENSIONS.get(self.classifier.type, ())
+        classifier = self.get_classifier()
+        required = classifier.REQUIRED_EXTENSIONS.get(classifier.type, ())
 
         if type(self) in required:
             raise ValidationError(
-                f"{type(self).__name__} extension is required for classifier type '{self.classifier.type}' "
+                f"{type(self).__name__} extension is required for classifier type '{classifier.type}' "
                 "and cannot be deleted independently"
             )
 
         return super().delete(*args, **kwargs)
 
     def __str__(self) -> str:
-        return f"{self.classifier}"
+        return f"{self.get_classifier()}"
 
 
-class NamedElement(ClassifierExtensionBase):
+class SingleClassifierExtensionBase(ClassifierExtensionBase):
+    classifier = models.OneToOneField(
+        Classifier,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="%(class)s",
+    )
+
+    class Meta:
+        abstract = True
+    
+    def get_classifier(self) -> "Classifier":
+        return self.classifier
+
+
+class MultiClassifierExtensionBase(ClassifierExtensionBase):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    classifier = models.ForeignKey(
+        Classifier,
+        on_delete=models.CASCADE,
+        related_name="%(class)ss",
+    )
+
+    class Meta:
+        abstract = True
+
+    def get_classifier(self) -> "Classifier":
+        return self.classifier
+
+
+class NamedElement(SingleClassifierExtensionBase):
     ALLOWED_CLASSIFIER_TYPES = (
         ClassifierType.CLASS,
         ClassifierType.ENUMERATION,
@@ -163,31 +249,42 @@ class NamedElement(ClassifierExtensionBase):
     name = models.CharField(max_length=255)
     namespace = models.CharField(max_length=255, blank=True, null=True)
 
-# Extension, however there can be multiple?
-# class Literals()
+
+class EnumLiteral(MultiClassifierExtensionBase):
+    ALLOWED_CLASSIFIER_TYPES = (ClassifierType.ENUMERATION,)
+    text = models.CharField(max_length=255)
 
 
-class ClassExtension(ClassifierExtensionBase):
+class ClassExtension(SingleClassifierExtensionBase):
     ALLOWED_CLASSIFIER_TYPES = (ClassifierType.CLASS,)
     abstract = models.BooleanField(default=False)
     leaf = models.BooleanField(default=False)
 
 
-class ClassAttribute(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+class ClassAttribute(MultiClassifierExtensionBase):
+    ALLOWED_CLASSIFIER_TYPES = (ClassifierType.CLASS,)
+
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=32, choices=AttributeType.choices)
-    derived = models.BooleanField(default=False)
+    derived=models.BooleanField(default=False)
     description = models.CharField(max_length=255, blank=True, null=True)
 
-    class_instance = models.ForeignKey(
-        ClassExtension, related_name="attributes", on_delete=models.CASCADE,
+    enum = models.ForeignKey(
+        Classifier,
+        on_delete=models.CASCADE,
+        related_name="enum_attributes",
+        null=True,
+        blank=True,
     )
-    # Relation to Enum, however, enum doesn't have any actual attributes
-    # Only relations to literals, however, i dont want to make a 
-    # Extension which is only referenced by the literals
-    # Directly putting the literals to the relation also doesn't work
-    # As it contains a one to one relation
-    # enum = models.ForeignKey(
 
-    # )
+    def clean(self):
+        super().clean()
+
+        if self.type == AttributeType.ENUM:
+            if self.enum is None:
+                raise ValidationError("Enum attribute must reference an enum classifier")
+            if self.enum.type != ClassifierType.ENUMERATION:
+                raise ValidationError("Enum attribute must reference an enumeration classifier")
+        else:
+            if self.enum is not None:
+                raise ValidationError("Only enum attributes can reference an enum classifier")
