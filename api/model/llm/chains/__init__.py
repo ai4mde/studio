d1a48fd07f79ac20d1a3570fb3b9a363f1c8b3fa
Xiangyu Li <lix0230@gmail.com>
2026-03-30 19:07:00 +0200

    chore(m05): export prose chain symbols

--- api/model/llm/chains/__init__.py
+++ api/model/llm/chains/__init__.py
@@ -1,7 +1,10 @@
 from .base import ChainResult, ChainRunner, ChainStep
+from .prose_chain import PROSE_CHAIN_STEPS, create_prose_chain_runner
 
 __all__ = [
     "ChainStep",
     "ChainResult",
     "ChainRunner",
+    "PROSE_CHAIN_STEPS",
+    "create_prose_chain_runner",
 ]
