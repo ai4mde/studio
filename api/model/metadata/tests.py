fe715603c8fe7dd79043515b34089338b9caa5c9
Irina Epure <irinamonaepure@gmail.com>
2026-04-27 21:58:28 +0200

    cd

--- api/model/metadata/tests.py
+++ api/model/metadata/tests.py
@@ -1,1 +1,19 @@
 # Create your tests here.
+
+from django.test import TestCase
+from metadata.models import Project, System
+
+class SystemModelTests(TestCase):
+    def test_create_system(self):
+        project = Project.objects.create(
+            name="Test Project",
+            description="Test description",
+        )
+        system = System.objects.create(
+            name="Test System",
+            description="System description",
+            project=project,
+        )
+
+        self.assertEqual(system.name, "Test System")
+        self.assertEqual(system.project.name, "Test Project")
