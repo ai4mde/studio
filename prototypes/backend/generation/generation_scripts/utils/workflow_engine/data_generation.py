b1357fb60e19031ce1184981d174d874b1d3a65d
MaartenV <maarten.verhoeff@caiway.net>
2026-05-12 14:36:06 +0200

    Update generation scripts to use metadata file paths

--- prototypes/backend/generation/generation_scripts/utils/workflow_engine/data_generation.py
+++ prototypes/backend/generation/generation_scripts/utils/workflow_engine/data_generation.py
@@ -75,12 +75,28 @@ class ActivityDiagramParser:
     @cached_property
     def interface_map(self) -> dict[str, str]:
         """Map from the action node UUID to a possible interface url"""
-        return {
-            page['action']['value']: f"/{app_name_sanitization(interface['value']['name'])}/render_{app_name_sanitization(interface['value']['name'])}_{page_name_sanitization(page['name'])}"
-            for interface in self.metadata['interfaces']
-            for page in interface['value']['data']['pages']
-            if page['type']['value'] != 'normal'
-        }
+        interface_map = {}
+
+        for interface in self.metadata['interfaces']:
+            interface_name = interface['value']['name']
+
+            for page in interface['value']['data']['pages']:
+                if page['type']['value'] == 'normal':
+                    continue
+
+                if page.get('action') is None:
+                    raise ValueError(
+                        f"Page '{page['name']}' in interface '{interface_name}' "
+                        f"is missing an action."
+                    )
+                
+                interface_map[page['action']['value']] = (
+                    f"/{app_name_sanitization(interface_name)}"
+                    f"/render_{app_name_sanitization(interface_name)}_"
+                    f"{page_name_sanitization(page['name'])}"
+                )
+
+        return interface_map
 
     def _get_incoming_edges_count(self, edges: list[dict[str, Any]], target_id: str) -> int:
         """Get the number of incoming edges for a node"""
