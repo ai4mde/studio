c55e6d9fe5e324163e7f32d259c5c0f744132ba2
MaartenV <maarten.verhoeff@caiway.net>
2026-06-04 14:22:02 +0200

    Revert "Allow for no text on section component"
    This reverts commit aaf91804188e7c37f643dd4d81fa494962b88ba9.

--- prototypes/backend/generation/generation_scripts/utils/loading_json_utils.py
+++ prototypes/backend/generation/generation_scripts/utils/loading_json_utils.py
@@ -223,7 +223,7 @@ def retrieve_section_components(application_name: str, page_name: str, metadata:
                         has_delete_operation = section["operations"]["delete"],
                         has_update_operation = section["operations"]["update"],
                         custom_methods = retrieve_section_custom_methods(section),
-                        text = section.get("text", "")
+                        text = section["text"]
                     )
                     out.append(sec)
             return out
