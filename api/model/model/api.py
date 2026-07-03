218511ae10eb24da1ab5c7ca0cdda253edb453c9
Jonne Goedhart <jonnegoedhart@gmail.com>
2026-07-03 11:52:53 +0200

    update backend packages

--- api/model/model/api.py
+++ api/model/model/api.py
@@ -12,8 +12,8 @@ api = NinjaAPI(
     version="0.0.1",  # TODO: Use package-wide versioning
     description="AI4MDE Studio API",
     auth=auth,
-    csrf=False,  # TODO: Ensure this works with Axios frontend / XSRF Header
 )
+
 api.add_router("/metadata/", metadata_router)
 api.add_router("/diagram/", diagram_router)
 api.add_router("/prose/", prose_router)
